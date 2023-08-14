fb0d701ec13fc7d9ef0328391c84833a393bf451
mgamboa <gamboa.martin@gmail.com>
2023-08-14 15:59:32 -0600

    v1.4.0 Se integra imp ubicacioon

---
+++ controllers/controlador_inm_ubicacion.php
@@ -0,0 +1,291 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\inmuebles\controllers;
+
+use base\controller\init;
+use gamboamartin\errores\errores;
+use gamboamartin\inmuebles\html\inm_ubicacion_html;
+use gamboamartin\inmuebles\models\inm_ubicacion;
+use gamboamartin\system\_ctl_base;
+use gamboamartin\system\links_menu;
+use gamboamartin\template\html;
+use PDO;
+use stdClass;
+
+class controlador_inm_ubicacion extends _ctl_base {
+
+    public function __construct(PDO      $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass())
+    {
+        $modelo = new inm_ubicacion(link: $link);
+        $html_ = new inm_ubicacion_html(html: $html);
+        $obj_link = new links_menu(link: $link, registro_id:  $this->registro_id);
+
+        $datatables = $this->init_datatable();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable',data: $datatables);
+            print_r($error);
+            die('Error');
+        }
+
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
+    }
+
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta = $this->init_alta();
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al inicializar alta',data:  $r_alta, header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_pais_descripcion');
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  array(), key: 'dp_pais_id',
+            keys_selects: array(), id_selected: 151, label: 'Pais', columns_ds : $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+        $filtro = array();
+        $filtro['dp_pais.id'] = 151;
+
+        $columns_ds = array('dp_estado_descripcion');
+
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_estado_id',
+            keys_selects: $keys_selects, id_selected: 14, label: 'Estado', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $filtro = array();
+        $filtro['dp_estado.id'] = 14;
+
+        $columns_ds = array('dp_municipio_descripcion');
+
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_municipio_id',
+            keys_selects: $keys_selects, id_selected: -1, label: 'Municipio', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_cp_descripcion');
+
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_cp_id',
+            keys_selects: $keys_selects, id_selected: -1, label: 'CP', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_colonia_descripcion');
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_colonia_postal_id',
+            keys_selects: $keys_selects, id_selected: -1, label: 'Colonia', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_calle_descripcion');
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_calle_pertenece_id',
+            keys_selects: $keys_selects, id_selected: -1, label: 'Calle', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+
+        $inputs = $this->inputs(keys_selects: $keys_selects);
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al obtener inputs',data:  $inputs, header: $header,ws:  $ws);
+        }
+
+        return $r_alta;
+    }
+
+    protected function campos_view(): array
+    {
+        $keys = new stdClass();
+        $keys->inputs = array('descripcion', 'manzana', 'lote','costo_directo','numero_exterior','numero_interior');
+        $keys->selects = array();
+
+
+        $init_data = array();
+        $init_data['dp_pais'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_estado'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_municipio'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_cp'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_colonia_postal'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_calle_pertenece'] = "gamboamartin\\direccion_postal";
+        $campos_view = $this->campos_view_base(init_data: $init_data,keys:  $keys);
+
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al inicializar campo view',data:  $campos_view);
+        }
+
+
+        return $campos_view;
+    }
+
+    protected function key_selects_txt(array $keys_selects): array
+    {
+
+        $keys_selects = (new init())->key_select_txt(cols: 6,key: 'descripcion', keys_selects:$keys_selects,
+            place_holder: 'Descripcion');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 6,key: 'manzana', keys_selects:$keys_selects,
+            place_holder: 'Manzana');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 6,key: 'lote', keys_selects:$keys_selects,
+            place_holder: 'Lote');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 12,key: 'costo_directo', keys_selects:$keys_selects,
+            place_holder: 'Costo Directo');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 6,key: 'numero_exterior', keys_selects:$keys_selects,
+            place_holder: 'Exterior');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 6,key: 'numero_interior', keys_selects:$keys_selects,
+            place_holder: 'Interior',required: false);
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+
+        return $keys_selects;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+
+        $r_modifica = $this->init_modifica(); // TODO: Change the autogenerated stub
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al generar salida de template',data:  $r_modifica,header: $header,ws: $ws);
+        }
+
+        $registro = $this->modelo->registro(registro_id: $this->registro_id,retorno_obj: true);
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al obtener registro',data:  $registro,header: $header,ws: $ws);
+        }
+
+
+        $columns_ds = array('dp_pais_descripcion');
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  array(), key: 'dp_pais_id',
+            keys_selects: array(), id_selected: $registro->dp_pais_id, label: 'Pais', columns_ds : $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects, header: $header,ws:  $ws);
+        }
+
+        $filtro = array();
+        $filtro['dp_pais.id'] = $registro->dp_pais_id;
+
+        $columns_ds = array('dp_estado_descripcion');
+
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_estado_id',
+            keys_selects: $keys_selects, id_selected: $registro->dp_estado_id, label: 'Estado', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+
+        $filtro = array();
+        $filtro['dp_estado.id'] = $registro->dp_estado_id;
+
+        $columns_ds = array('dp_municipio_descripcion');
+
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_municipio_id',
+            keys_selects: $keys_selects, id_selected: $registro->dp_municipio_id, label: 'Municipio', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_cp_descripcion');
+
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_cp_id',
+            keys_selects: $keys_selects, id_selected: $registro->dp_cp_id, label: 'CP', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_colonia_descripcion');
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_colonia_postal_id',
+            keys_selects: $keys_selects, id_selected: $registro->dp_colonia_postal_id, label: 'Colonia', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $columns_ds = array('dp_calle_descripcion');
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  $filtro, key: 'dp_calle_pertenece_id',
+            keys_selects: $keys_selects, id_selected: $registro->dp_calle_pertenece_id, label: 'Calle', columns_ds: $columns_ds);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects,
+                header: $header,ws:  $ws);
+        }
+
+        $base = $this->base_upd(keys_selects: $keys_selects, params: array(),params_ajustados: array());
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al integrar base',data:  $base, header: $header,ws:  $ws);
+        }
+
+
+
+        return $r_modifica;
+    }
+
+    /**
+     * Inicializa los elementos mostrables para datatables
+     * @return stdClass
+     */
+    private function init_datatable(): stdClass
+    {
+        $columns["inm_ubicacion_id"]["titulo"] = "Id";
+        $columns["dp_municipio_descripcion"]["titulo"] = "Municipio";
+        $columns["dp_cp_descripcion"]["titulo"] = "CP";
+        $columns["dp_colonia_descripcion"]["titulo"] = "Colonia";
+        $columns["dp_calle_descripcion"]["titulo"] = "Calle";
+        $columns["inm_ubicacion_numero_exterior"]["titulo"] = "Ext";
+        $columns["inm_ubicacion_numero_interior"]["titulo"] = "Int";
+        $columns["inm_ubicacion_manzana"]["titulo"] = "Manzana";
+        $columns["inm_ubicacion_lote"]["titulo"] = "Lote";
+
+        $filtro = array("inm_ubicacion.id","dp_municipio.descripcion",'dp_cp.descripcion','dp_colonia.descripcion',
+            'dp_calle.descripcion','inm_ubicacion.numero_exterior','inm_ubicacion.numero_interior',
+            'inm_ubicacion.manzana','inm_ubicacion.lote');
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        return $datatables;
+    }
+
+
+}
---
+++ templates/directivas/inm_ubicacion_html.php
@@ -0,0 +1,26 @@
+<?php
+namespace gamboamartin\inmuebles\html;
+use gamboamartin\errores\errores;
+use gamboamartin\inmuebles\models\inm_producto_infonavit;
+use gamboamartin\inmuebles\models\inm_ubicacion;
+use gamboamartin\system\html_controler;
+use PDO;
+use stdClass;
+
+class inm_ubicacion_html extends html_controler {
+
+    public function select_inm_ubicacion_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                      bool $disabled = false, array $filtro = array()): array|string
+    {
+        $modelo = new inm_ubicacion(link: $link);
+
+        $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
+            modelo: $modelo, disabled: $disabled, filtro: $filtro, label: 'Ubicacion', required: true);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+
+
+}
