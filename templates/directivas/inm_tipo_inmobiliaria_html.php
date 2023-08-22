3344729543b95793fb10ac3f7c07d54ba7e27ee1
mgamboa <gamboa.martin@gmail.com>
2023-08-22 09:33:52 -0600

    v1.27.0 CRUD tipo inmobiliaria

--- templates/directivas/inm_tipo_inmobiliaria_html.php
+++ templates/directivas/inm_tipo_inmobiliaria_html.php
@@ -1,20 +1,20 @@
 <?php
 namespace gamboamartin\inmuebles\html;
 use gamboamartin\errores\errores;
-use gamboamartin\inmuebles\models\inm_destino_credito;
+use gamboamartin\inmuebles\models\inm_tipo_inmobiliaria;
 use gamboamartin\system\html_controler;
 use PDO;
 use stdClass;
 
-class inm_destino_credito_html extends html_controler {
+class inm_tipo_inmobiliaria_html extends html_controler {
 
-    public function select_destino_credito_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+    public function select_tipo_inmobiliaria_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
                                       bool $disabled = false, array $filtro = array()): array|string
     {
-        $modelo = new inm_destino_credito(link: $link);
+        $modelo = new inm_tipo_inmobiliaria(link: $link);
 
         $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
-            modelo: $modelo, disabled: $disabled, filtro: $filtro, label: 'Destino Credito', required: true);
+            modelo: $modelo, disabled: $disabled, filtro: $filtro, label: 'Tipo Inmobiliaria', required: true);
         if (errores::$error) {
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
