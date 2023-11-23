8a239e3215fcb444bf25971406d971a22fc87b17
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-11-23 15:21:20 -0600

    v2.330.2 Se integra ajusta vista de co

---
+++ controllers/controlador_comi_comision.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\inmuebles\controllers;
+class controlador_comi_comision extends \gamboamartin\comisiones\controllers\controlador_comi_comision {}
