53698020918930b1473fe8660addbe1d30f5086e
mgamboa <gamboa.martin@gmail.com>
2024-04-15 15:11:07 -0600

    v4.2.0 Se integra mejoras de servicios oen inmuebles

--- __inicializa.php
+++ __inicializa.php
@@ -16,72 +16,80 @@ $administrador = new gamboamartin\administrador\instalacion\instalacion();
 
 $instala = $administrador->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
-    $error = (new errores())->error(mensaje: 'Error al instalar administrador', data: $administrador);
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
+    $error = (new errores())->error(mensaje: 'Error al instalar administrador', data: $instala);
     print_r($error);
     exit;
 }
 
-//print_r($instala);
 
 
 $cat_sat = new gamboamartin\cat_sat\instalacion\instalacion();
 
 $instala = $cat_sat->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
     $error = (new errores())->error(mensaje: 'Error al instalar cat_sat', data: $instala);
     print_r($error);
     exit;
 }
-//print_r($instala);
 
 $organigrama = new gamboamartin\organigrama\instalacion\instalacion();
 
 $instala = $organigrama->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
     $error = (new errores())->error(mensaje: 'Error al instalar organigrama', data: $instala);
     print_r($error);
     exit;
 }
-//print_r($instala);
 
 
 $comercial = new gamboamartin\comercial\instalacion\instalacion();
 
 $instala = $comercial->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
     $error = (new errores())->error(mensaje: 'Error al instalar comercial', data: $instala);
     print_r($error);
     exit;
 }
 
-//print_r($instala);
 
 $facturacion = new gamboamartin\facturacion\instalacion\instalacion();
 
 $instala = $facturacion->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
     $error = (new errores())->error(mensaje: 'Error al instalar facturacion', data: $instala);
     print_r($error);
     exit;
 }
 
-//print_r($instala);
 
 $inmuebles = new gamboamartin\inmuebles\instalacion\instalacion();
 
 $instala = $inmuebles->instala(link: $link);
 if(errores::$error){
-    $link->rollBack();
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
     $error = (new errores())->error(mensaje: 'Error al instalar inmuebles', data: $instala);
     print_r($error);
     exit;
 }
 
-//print_r($instala);
 
-$link->commit();
+if($link->inTransaction()) {
+    $link->commit();
+}
