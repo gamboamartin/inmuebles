dc12b3836e9b32b33478e486603a31854a735231
Kevin <kevin.acuna@ivitec.com.mx>
2024-06-10 14:41:29 -0500

    v4.68.5 Se integra func. para receptor

--- orm/_email.php
+++ orm/_email.php
@@ -3,7 +3,9 @@
 namespace gamboamartin\inmuebles\models;
 
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use gamboamartin\notificaciones\models\not_emisor;
+use gamboamartin\notificaciones\models\not_receptor;
 use stdClass;
 
 class _email
@@ -29,12 +31,44 @@ class _email
             return $this->modelo->error->error(mensaje: "Error al filtrar al emisor", data: $not_emisor);
         }
 
-        if($not_emisor->nro_regisros == 0){
+        if ($not_emisor->nro_regisros == 0) {
             return $this->modelo->error->error(mensaje: "No se encontró el emisor con el correo '$correo'",
                 data: $not_emisor);
         }
 
         return $not_emisor->registros[0]['not_emisor_id'];
+    }
+
+    public function receptor(string $correo): array
+    {
+        $validacion = $this->validar_correo($correo);
+        if (!$validacion) {
+            return $this->modelo->error->error(mensaje: "El correo '$correo' no es válido.", data: $correo);
+        }
+
+        $filtro = array();
+        $filtro['email'] = $correo;
+        $not_receptor = (new not_receptor(link: $this->modelo->link))->filtro_and(filtro: $filtro);
+        if (errores::$error) {
+            return $this->modelo->error->error(mensaje: "Error al filtrar al emisor", data: $not_receptor);
+        }
+
+        if ($not_receptor->nro_regisros == 0) {
+            $alta_not_receptor = (new not_receptor(link: $this->modelo->link))->alta_registro(
+                array(
+                    'email' => $correo,
+                    'descripcion' => $correo,
+                    'descripcion' => $correo,
+                    'codigo' => $correo,
+                ));
+            if (errores::$error) {
+                return $this->modelo->error->error(mensaje: "Error al insertar al receptor", data: $alta_not_receptor);
+            }
+
+            return $alta_not_receptor->registro_id;
+        }
+
+        return $not_receptor->registros[0]['not_receptor_id'];
     }
 
     public function validar_correo($correo): mixed
