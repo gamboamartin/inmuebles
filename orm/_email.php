79eabe91db3ebf837a4c52f1b20c22e1325d56c5
Kevin <kevin.acuna@ivitec.com.mx>
2024-06-11 14:35:59 -0500

    v4.71.5 Se integran UTs

--- orm/_email.php
+++ orm/_email.php
@@ -6,20 +6,22 @@ use base\orm\modelo;
 use gamboamartin\errores\errores;
 use gamboamartin\notificaciones\models\not_emisor;
 use gamboamartin\notificaciones\models\not_receptor;
+use PDO;
 use stdClass;
 
 class _email
 {
-    public modelo $modelo;
-
     public const ERROR_CORREO_NO_VALIDO = "El correo '%s' no es válido.";
+    public const ERROR_CORREO_NO_EXISTE = "El correo '%s' no es existe.";
     public const ERROR_FILTRO = "Error al filtrar al '%s'";
     public const ERROR_VALIDACION = "Error al validar el correo '%s'";
     public const ERROR_CORREO_NO_ENCONTRADO = "No se encontró el emisor con el correo '%s'";
+    public const ERROR_AL_INSERTAR = "Error al insertar al '%s'";
+    private PDO $link;
 
-    public function __construct(modelo $modelo)
+    public function __construct(PDO $link)
     {
-        $this->modelo = $modelo;
+        $this->link = $link;
     }
 
     public function correo_validacion(string $correo, modelo $modelo, string $campo): array|stdClass
@@ -27,7 +29,7 @@ class _email
         $validacion = $this->validar_correo($correo);
         if (!$validacion) {
             $mensaje_error = sprintf(self::ERROR_CORREO_NO_VALIDO, $correo);
-            return $this->modelo->error->error(mensaje: $mensaje_error, data: $correo);
+            return (new errores())->error(mensaje: $mensaje_error, data: $correo);
         }
 
         $filtro = array();
@@ -35,7 +37,7 @@ class _email
         $datos = $modelo->filtro_and(filtro: $filtro);
         if (errores::$error) {
             $mensaje_error = sprintf(self::ERROR_FILTRO, $campo);
-            return $this->modelo->error->error(mensaje: $mensaje_error, data: $datos);
+            return (new errores())->error(mensaje: $mensaje_error, data: $datos);
         }
 
         return $datos;
@@ -43,15 +45,15 @@ class _email
 
     public function emisor(string $correo): array
     {
-        $datos = $this->correo_validacion(correo: $correo, modelo: (new not_emisor(link: $this->modelo->link)), campo: 'emisor');
+        $datos = $this->correo_validacion(correo: $correo, modelo: (new not_emisor(link: $this->link)), campo: 'emisor');
         if (errores::$error) {
             $mensaje_error = sprintf(self::ERROR_VALIDACION, $correo);
-            return $this->modelo->error->error(mensaje: $mensaje_error, data: $datos);
+            return (new errores())->error(mensaje: $mensaje_error, data: $datos);
         }
 
         if ($datos->n_registros == 0) {
-            $mensaje_error = sprintf(self::ERROR_CORREO_NO_VALIDO, $correo);
-            return $this->modelo->error->error(mensaje: $mensaje_error, data: $datos);
+            $mensaje_error = sprintf(self::ERROR_CORREO_NO_EXISTE, $correo);
+            return (new errores())->error(mensaje: $mensaje_error, data: $datos);
         }
 
         return $datos->registros[0];
@@ -59,14 +61,14 @@ class _email
 
     public function receptor(string $correo): array
     {
-        $datos = $this->correo_validacion(correo: $correo, modelo: (new not_receptor(link: $this->modelo->link)), campo: 'receptor');
+        $datos = $this->correo_validacion(correo: $correo, modelo: (new not_receptor(link: $this->link)), campo: 'receptor');
         if (errores::$error) {
             $mensaje_error = sprintf(self::ERROR_VALIDACION, $correo);
-            return $this->modelo->error->error(mensaje: $mensaje_error, data: $datos);
+            return (new errores())->error(mensaje: $mensaje_error, data: $datos);
         }
 
         if ($datos->n_registros == 0) {
-            $alta_not_receptor = (new not_receptor(link: $this->modelo->link))->alta_registro(
+            $alta_not_receptor = (new not_receptor(link: $this->link))->alta_registro(
                 array(
                     'email' => $correo,
                     'descripcion' => $correo,
@@ -74,10 +76,11 @@ class _email
                     'codigo' => $correo,
                 ));
             if (errores::$error) {
-                return $this->modelo->error->error(mensaje: "Error al insertar al receptor", data: $alta_not_receptor);
+                $mensaje_error = sprintf(self::ERROR_AL_INSERTAR, 'receptor');
+                return (new errores())->error(mensaje: $mensaje_error, data: $alta_not_receptor);
             }
 
-            return (new not_receptor(link: $this->modelo->link))->registro(registro_id: $alta_not_receptor->registro_id);
+            return (new not_receptor(link: $this->link))->registro(registro_id: $alta_not_receptor->registro_id);
         }
 
         return $datos->registros[0];
--- tests/orm/_emailTest.php
+++ tests/orm/_emailTest.php
@@ -1,16 +1,21 @@
 <?php
+
 namespace gamboamartin\inmuebles\tests\models;
 
+use gamboamartin\comercial\models\com_tipo_cliente;
 use gamboamartin\errores\errores;
 use gamboamartin\inmuebles\models\_email;
-use gamboamartin\inmuebles\models\inm_prospecto;
+use gamboamartin\notificaciones\models\not_emisor;
+use gamboamartin\notificaciones\models\not_receptor;
 use gamboamartin\test\test;
 
 use stdClass;
 
-class _emailTest extends test {
+class _emailTest extends test
+{
     public errores $errores;
     private stdClass $paths_conf;
+
     public function __construct(?string $name = null)
     {
         parent::__construct($name);
@@ -21,7 +26,7 @@ class _emailTest extends test {
         $this->paths_conf->views = '/var/www/html/inmuebles/config/views.php';
     }
 
-    public function test_emisor(): void
+    public function test_correo_validacion(): void
     {
         errores::$error = false;
 
@@ -32,12 +37,50 @@ class _emailTest extends test {
         $_GET['session_id'] = '1';
 
         $correo = "test";
-        $resultado = (new _email(modelo: (new inm_prospecto($this->link))))->emisor(correo: $correo);
+        $resultado = (new _email(link: $this->link))
+            ->correo_validacion(correo: $correo,
+                modelo: (new not_emisor($this->link)),
+                campo: 'emisor');
         $mensaje_error = sprintf(_email::ERROR_CORREO_NO_VALIDO, $correo);
         $this->assertEquals($resultado['mensaje_limpio'], $mensaje_error);
-
         errores::$error = false;
 
+        $correo = "xxxxx@ivitec.mx";
+        $resultado = (new _email(link: $this->link))
+            ->correo_validacion(correo: $correo,
+                modelo: (new com_tipo_cliente($this->link)),
+                campo: 'emisor');
+        $mensaje_error = sprintf(_email::ERROR_FILTRO, 'emisor');
+        $this->assertEquals($resultado['mensaje_limpio'], $mensaje_error);
+        errores::$error = false;
+    }
+
+    public function test_emisor(): void
+    {
+        $this->test_correo_validacion();
+
+        $correo = "xxxxx@ivitec.mx";
+        $resultado = (new _email(link: $this->link))->emisor(correo: $correo);
+        $mensaje_error = sprintf(_email::ERROR_CORREO_NO_EXISTE, $correo);
+        $this->assertEquals($resultado['mensaje_limpio'], $mensaje_error);
+        errores::$error = false;
+
+        $correo = "test@ivitec.mx";
+        $resultado = (new _email(link: $this->link))->emisor(correo: $correo);
+        $this->assertIsArray($resultado);
+        $this->assertGreaterThan(0, $resultado);
+        errores::$error = false;
+    }
+
+    public function test_receptor(): void
+    {
+        $this->test_correo_validacion();
+
+        $correo = "test@ivitec.mx";
+        $resultado = (new _email(link: $this->link))->receptor(correo: $correo);
+        $this->assertIsArray($resultado);
+        $this->assertGreaterThan(0, $resultado);
+        errores::$error = false;
     }
 }
 
