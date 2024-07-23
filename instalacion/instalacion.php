8733ba2632af7a6c405ee2af0a79a2c00adeef61
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-07-23 12:11:06 -0600

    v4.105.5 Se integra crud de inm prototipo

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -364,6 +364,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_prototipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_prototipo');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function inm_comprador(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -710,6 +724,35 @@ class instalacion
         return $out;
 
     }
+    private function inm_prototipo(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+
+        $create = $this->_add_inm_prototipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+
+        $out->create = $create;
+
+        $adm_menu_descripcion = 'Parametros Infonavit';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'Prototipo';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+        return $out;
+
+    }
 
     final public function instala(PDO $link): array|stdClass
     {
@@ -720,6 +763,12 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_sindicato', data:  $inm_sindicato);
         }
         $out->inm_sindicato = $inm_sindicato;
+
+        $inm_prototipo = $this->inm_prototipo(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_prototipo', data:  $inm_prototipo);
+        }
+        $out->inm_prototipo = $inm_prototipo;
 
         $inm_prospecto = $this->inm_prospecto(link: $link);
         if(errores::$error){
