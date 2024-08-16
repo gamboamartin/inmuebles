5b3e919114a5858db71a6db6c81915d75fb0095d
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-16 16:13:43 -0600

    v4.136.6 Se registra inm_conf_docs_prospecto en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -591,6 +591,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_conf_docs_prospecto(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_conf_docs_prospecto');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function inm_comprador(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -1407,6 +1421,67 @@ class instalacion
 
     }
 
+    private function inm_conf_docs_prospecto(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_conf_docs_prospecto(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+        $columnas = new stdClass();
+        $columnas->es_obligatorio = new stdClass();
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_entidad = $add_colums;
+
+        $foraneas = array();
+        $foraneas['doc_tipo_documento_id'] = new stdClass();
+        $foraneas['inm_attr_tipo_credito_id'] = new stdClass();
+        $foraneas['inm_destino_credito_id'] = new stdClass();
+        $foraneas['inm_producto_infonavit_id'] = new stdClass();
+        $foraneas['pr_sub_proceso_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+        $out->foraneas = $result;
+
+
+        $adm_menu_descripcion = 'Clientes';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'Documentos Prospecto';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
     private function inm_condicion_vivienda(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -1470,6 +1545,12 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_comprador_proceso', data:  $inm_comprador_proceso);
         }
         $out->inm_comprador_proceso = $inm_comprador_proceso;
+
+        $inm_conf_docs_prospecto = $this->inm_conf_docs_prospecto(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_conf_docs_prospecto', data:  $inm_conf_docs_prospecto);
+        }
+        $out->inm_conf_docs_prospecto = $inm_conf_docs_prospecto;
 
         $inm_concepto = $this->inm_concepto(link: $link);
         if(errores::$error){
