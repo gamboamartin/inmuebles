bcf93ff3b5799f4aece0820614724aeb74a67b37
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-16 17:10:21 -0600

    v4.140.6 Se registra inm_conyuge en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -633,6 +633,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_conyuge(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_conyuge');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function _add_inm_conf_empresa(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -1683,6 +1697,72 @@ class instalacion
 
     }
 
+    private function inm_conyuge(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_conyuge(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+        $columnas = new stdClass();
+        $columnas->nmbre = new stdClass();
+        $columnas->apellido_paterno = new stdClass();
+        $columnas->apellido_materno = new stdClass();
+        $columnas->curp = new stdClass();
+        $columnas->rfc = new stdClass();
+        $columnas->telefono_casa = new stdClass();
+        $columnas->telefono_celular = new stdClass();
+        $columnas->fecha_nacimiento = new stdClass();
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_entidad = $add_colums;
+
+        $foraneas = array();
+        $foraneas['dp_municipio_id'] = new stdClass();
+        $foraneas['inm_nacionalidad_id'] = new stdClass();
+        $foraneas['inm_ocupacion_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+        $out->foraneas = $result;
+
+
+        $adm_menu_descripcion = 'Parametros Infonavit';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'Conyuge';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
     private function inm_condicion_vivienda(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -1770,6 +1850,12 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_conf_empresa', data:  $inm_conf_empresa);
         }
         $out->inm_conf_empresa = $inm_conf_empresa;
+
+        $inm_conyuge = $this->inm_conyuge(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_conyuge', data:  $inm_conyuge);
+        }
+        $out->inm_conyuge = $inm_conyuge;
 
         $inm_concepto = $this->inm_concepto(link: $link);
         if(errores::$error){
