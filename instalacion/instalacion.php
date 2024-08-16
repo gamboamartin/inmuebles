f02b53930ea72bcc9fabbf924bf9d69a079d767d
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-16 17:44:28 -0600

    v4.145.6 Se registra inm_estado_civil en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -703,6 +703,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_estado_civil(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_estado_civil');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function _add_inm_conf_empresa(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -2021,6 +2035,54 @@ class instalacion
 
     }
 
+    private function inm_estado_civil(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_estado_civil(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+        $columnas = new stdClass();
+        $columnas->x = new stdClass();
+        $columnas->y = new stdClass();
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_entidad = $add_colums;
+
+        $adm_menu_descripcion = 'Parametros Infonavit';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'Estado Civil';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
     private function inm_condicion_vivienda(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -2138,6 +2200,12 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_doc_prospecto_ubicacion', data:  $inm_doc_prospecto_ubicacion);
         }
         $out->inm_doc_prospecto_ubicacion = $inm_doc_prospecto_ubicacion;
+
+        $inm_estado_civil = $this->inm_estado_civil(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_estado_civil', data:  $inm_estado_civil);
+        }
+        $out->inm_estado_civil = $inm_estado_civil;
 
         $inm_concepto = $this->inm_concepto(link: $link);
         if(errores::$error){
