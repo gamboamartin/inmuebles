6a4a9266da2cb6544c472300b1ee6151f5f3895b
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-16 16:51:32 -0600

    v4.138.6 Se registra inm_conf_empresa en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -619,6 +619,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_conf_empresa(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'conf_empresa');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function inm_comprador(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -1555,6 +1569,48 @@ class instalacion
 
     }
 
+    private function inm_conf_empresa(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_conf_empresa(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $foraneas = array();
+        $foraneas['org_empresa_id'] = new stdClass();
+        $foraneas['inm_tipo_inmobiliaria_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+        $out->foraneas = $result;
+
+
+        $adm_menu_descripcion = 'Generales';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'Conf Empresa';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
     private function inm_condicion_vivienda(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -1624,6 +1680,18 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_conf_docs_prospecto', data:  $inm_conf_docs_prospecto);
         }
         $out->inm_conf_docs_prospecto = $inm_conf_docs_prospecto;
+
+        $inm_conf_docs_prospecto_ubicacion = $this->inm_conf_docs_prospecto_ubicacion(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_conf_docs_prospecto_ubicacion', data:  $inm_conf_docs_prospecto_ubicacion);
+        }
+        $out->inm_conf_docs_prospecto_ubicacion = $inm_conf_docs_prospecto_ubicacion;
+
+        $inm_conf_empresa = $this->inm_conf_empresa(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_conf_empresa', data:  $inm_conf_empresa);
+        }
+        $out->inm_conf_empresa = $inm_conf_empresa;
 
         $inm_concepto = $this->inm_concepto(link: $link);
         if(errores::$error){
