bc4f2cad0feb84974d1493b7cecad555b3bb38ca
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-26 16:03:18 -0600

    v4.151.6 se registra inm_opinion_valor en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -759,6 +759,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_opinion_valor(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_opinion_valor');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function _add_inm_conf_empresa(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -2198,9 +2212,69 @@ class instalacion
         }
         $out->create = $create;
 
-        $adm_menu_descripcion = 'Parametros Infonavit';
+        $adm_menu_descripcion = 'Generales';
         $adm_sistema_descripcion = 'inmuebles';
         $etiqueta_label = 'Ocupacion';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
+    private function inm_opinion_valor(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_opinion_valor(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+        $columnas = new stdClass();
+        $columnas->fecha = new stdClass();
+        $columnas->monto_resultado = new stdClass();
+        $columnas->costo = new stdClass();
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_entidad = $add_colums;
+
+        $foraneas = array();
+        $foraneas['inm_ubicacion_id'] = new stdClass();
+        $foraneas['inm_valuador_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+        $out->foraneas = $result;
+
+
+        $adm_menu_descripcion = 'Parametros Infonavit';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'Opinion Valor';
         $adm_seccion_pertenece_descripcion = 'inmuebles';
         $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
         $adm_namespace_name = 'gamboamartin/inmuebles';
@@ -2359,6 +2433,12 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_ocupacion', data:  $inm_ocupacion);
         }
         $out->inm_ocupacion = $inm_ocupacion;
+
+        $inm_opinion_valor = $this->inm_opinion_valor(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_opinion_valor', data:  $inm_opinion_valor);
+        }
+        $out->inm_opinion_valor = $inm_opinion_valor;
 
         $inm_concepto = $this->inm_concepto(link: $link);
         if(errores::$error){
