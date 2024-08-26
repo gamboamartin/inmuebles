c89b67b4ac48a209202ddb2f9fab77001b31285d
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-26 17:19:39 -0600

    v4.158.6 se registra inm_referencia_prospecto en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -801,6 +801,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_referencia_prospecto(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_referencia_prospecto');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function _add_inm_prospecto_ubicacion_proceso(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -2534,6 +2548,71 @@ class instalacion
 
     }
 
+    private function inm_referencia_prospecto(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_referencia_prospecto(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+        $columnas = new stdClass();
+        $columnas->apellido_paterno = new stdClass();
+        $columnas->apellido_materno = new stdClass();
+        $columnas->nombre = new stdClass();
+        $columnas->lada = new stdClass();
+        $columnas->numero = new stdClass();
+        $columnas->celular = new stdClass();
+        $columnas->numero_dom = new stdClass();
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_entidad = $add_colums;
+
+        $foraneas = array();
+        $foraneas['dp_calle_pertenece_id'] = new stdClass();
+        $foraneas['inm_prospecto_id'] = new stdClass();
+        $foraneas['inm_parentesco_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+        $out->foraneas = $result;
+
+
+        $adm_menu_descripcion = 'Clientes';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'referencia prospecto';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
     private function inm_prospecto_ubicacion_proceso(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -2986,11 +3065,11 @@ class instalacion
         }
         $out->inm_referencia = $inm_referencia;
 
-        $inm_referencia = $this->inm_referencia(link: $link);
+        $inm_referencia_prospecto = $this->inm_referencia_prospecto(link: $link);
         if(errores::$error){
-            return (new errores())->error(mensaje: 'Error integrar inm_referencia', data:  $inm_referencia);
+            return (new errores())->error(mensaje: 'Error integrar inm_referencia_prospecto', data:  $inm_referencia_prospecto);
         }
-        $out->inm_referencia = $inm_referencia;
+        $out->inm_referencia_prospecto = $inm_referencia_prospecto;
 
         $inm_prospecto_ubicacion_proceso = $this->inm_prospecto_ubicacion_proceso(link: $link);
         if(errores::$error){
