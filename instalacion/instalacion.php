c1f3d303584d8aa607fe2703e19aa29598c855a4
samuelalexsanch3 <samuel.garcia@ivitec.com.mx>
2024-08-27 19:09:25 -0600

    v4.171.6 se registra inm_ubicacion_etapa en instalador

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -787,6 +787,20 @@ class instalacion
         return $out;
     }
 
+    private function _add_inm_ubicacion_etapa(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $init->create_table_new(table: 'inm_ubicacion_etapa');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        return $out;
+    }
+
     private function _add_inm_ubicacion(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -2651,6 +2665,64 @@ class instalacion
 
     }
 
+    private function inm_ubicacion_etapa(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $init = (new _instalacion(link: $link));
+
+        $create = $this->_add_inm_ubicacion_etapa(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
+        }
+        $out->create = $create;
+
+        $columnas = new stdClass();
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_base = $add_colums;
+
+        $columnas = new stdClass();
+        $columnas->fecha = new stdClass();
+
+        $add_colums = $init->add_columns(campos: $columnas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al agregar columnas', data:  $add_colums);
+        }
+        $out->add_colums_entidad = $add_colums;
+
+        $foraneas = array();
+        $foraneas['inm_ubicacion_id'] = new stdClass();
+        $foraneas['pr_etapa_proceso_id'] = new stdClass();
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  __FUNCTION__);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+        $out->foraneas = $result;
+
+
+        $adm_menu_descripcion = 'Ubicacion';
+        $adm_sistema_descripcion = 'inmuebles';
+        $etiqueta_label = 'ubicacion etapa';
+        $adm_seccion_pertenece_descripcion = 'inmuebles';
+        $adm_namespace_descripcion = 'gamboa.martin/inmuebles';
+        $adm_namespace_name = 'gamboamartin/inmuebles';
+
+        $acl = (new _adm())->integra_acl(adm_menu_descripcion: $adm_menu_descripcion,
+            adm_namespace_name: $adm_namespace_name, adm_namespace_descripcion: $adm_namespace_descripcion,
+            adm_seccion_descripcion: __FUNCTION__, adm_seccion_pertenece_descripcion: $adm_seccion_pertenece_descripcion,
+            adm_sistema_descripcion: $adm_sistema_descripcion, etiqueta_label: $etiqueta_label, link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener acl', data:  $acl);
+        }
+
+
+        return $out;
+
+    }
+
     private function inm_ubicacion(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -3779,6 +3851,12 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar inm_precio', data:  $inm_precio);
         }
         $out->inm_precio = $inm_precio;
+
+        $inm_ubicacion_etapa = $this->inm_ubicacion_etapa(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error integrar inm_ubicacion_etapa', data:  $inm_ubicacion_etapa);
+        }
+        $out->inm_ubicacion_etapa = $inm_ubicacion_etapa;
 
         $inm_ubicacion = $this->inm_ubicacion(link: $link);
         if(errores::$error){
