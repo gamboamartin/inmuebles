e7f30f72c647904ae4986e1650ea64bd5ff465a1
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-09-25 22:40:33 -0600

    v4.194.6 Se ajusta contenido de css de proceso

--- js/inm_comprador/proceso_cliente.js
+++ js/inm_comprador/proceso_cliente.js
@@ -4,17 +4,14 @@ let session_id = getParameterByName('session_id');
 
 function cambiarPestanna(pestannas,pestanna) {
 
-    // Obtiene los elementos con los identificadores pasados.
     pestanna = document.getElementById(pestanna.id);
     listaPestannas = document.getElementById(pestannas.id);
 
-    // Obtiene las divisiones que tienen el contenido de las pestañas.
     cpestanna = document.getElementById('c'+pestanna.id);
     listacPestannas = document.getElementById('contenido'+pestannas.id);
 
     i=0;
-    // Recorre la lista ocultando todas las pestañas y restaurando el fondo
-    // y el padding de las pestañas.
+
     while (typeof listacPestannas.getElementsByTagName('div')[i] != 'undefined'){
         $(document).ready(function(){
             $(listacPestannas.getElementsByTagName('div')[i]).css('display','none');
@@ -25,13 +22,8 @@ function cambiarPestanna(pestannas,pestanna) {
     }
 
     $(document).ready(function(){
-        // Muestra el contenido de la pestaña pasada como parametro a la funcion,
-        // cambia el color de la pestaña y aumenta el padding para que tape el
-        // borde superior del contenido que esta juesto debajo y se vea de este
-        // modo que esta seleccionada.
         $(cpestanna).css('display','');
-        $(pestanna).css('background','dimgray');
-        $(pestanna).css('padding-bottom','2px');
+        $(pestanna).css('background','#0f7ad5');
     });
 
 }
--- views/inm_comprador/proceso_cliente.php
+++ views/inm_comprador/proceso_cliente.php
@@ -9,11 +9,22 @@
             <div class="col-lg-12">
 
                 <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
+                    <?php include (new views())->ruta_templates."head/title.php"; ?>
+                    <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
+                    <?php include (new views())->ruta_templates."mensajes.php"; ?>
 
                     <div id="pestanas">
                         <ul id=lista>
-                            <li id="pestana1"><a href='javascript:cambiarPestanna(pestanas,pestana1);'>HTML</a></li>
-                            <li id="pestana2"><a href='javascript:cambiarPestanna(pestanas,pestana2);'>CSS</a></li>
+                            <li id="pestana1"><a href='javascript:cambiarPestanna(pestanas,pestana1);'>DETENIDO</a></li>
+                            <li id="pestana2"><a href='javascript:cambiarPestanna(pestanas,pestana2);'>ASIGNADO</a></li>
+                            <li id="pestana3"><a href='javascript:cambiarPestanna(pestanas,pestana3);'>EN AVALUO</a></li>
+                            <li id="pestana4"><a href='javascript:cambiarPestanna(pestanas,pestana4);'>POR INGRESAR</a></li>
+                            <li id="pestana5"><a href='javascript:cambiarPestanna(pestanas,pestana5);'>INGRESADO</a></li>
+                            <li id="pestana6"><a href='javascript:cambiarPestanna(pestanas,pestana6);'>AUTORIZADO</a></li>
+                            <li id="pestana7"><a href='javascript:cambiarPestanna(pestanas,pestana7);'>POR FIRMAR</a></li>
+                            <li id="pestana8"><a href='javascript:cambiarPestanna(pestanas,pestana8);'>ESCRITURADO</a></li>
+                            <li id="pestana9"><a href='javascript:cambiarPestanna(pestanas,pestana9);'>COTEJADO</a></li>
+                            <li id="pestana10"><a href='javascript:cambiarPestanna(pestanas,pestana10);'>COTEJO APROBADO</a></li>
                         </ul>
                     </div>
                     <body onload="javascript:cambiarPestanna(pestanas,pestana1);">
@@ -24,7 +35,32 @@
                         <div id="cpestana2">
                             Contenido de la pestaña 2
                         </div>
-                    </div>
+                        <div id="cpestana3">
+                            Contenido de la pestaña 1
+                        </div>
+                        <div id="cpestana4">
+                            Contenido de la pestaña 2
+                        </div>
+                        <div id="cpestana5">
+                            Contenido de la pestaña 1
+                        </div>
+                        <div id="cpestana6">
+                            Contenido de la pestaña 2
+                        </div>
+                        <div id="cpestana7">
+                            Contenido de la pestaña 1
+                        </div>
+                        <div id="cpestana8">
+                            Contenido de la pestaña 2
+                        </div>
+                        <div id="cpestana9">
+                            Contenido de la pestaña 1
+                        </div>
+                        <div id="cpestana10">
+                            Contenido de la pestaña 2
+                        </div>
+
+
                 </div>
             </div>
         </div>
